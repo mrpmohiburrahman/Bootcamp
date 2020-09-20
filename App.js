31684fec5528623822cab88215e9b4dddd9bd895
saad <saad@socar.my>
2020-09-20 12:30:24 +0800

    boilerplate added

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
+import RootNavigation from "./src/navigation/RootNavigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded] = useFonts({
+    EncodeSansRegular: require("./assets/fonts/EncodeSans-Regular.ttf"),
+    EncodeSansBold: require("./assets/fonts/EncodeSans-Bold.ttf"),
+    MontserratRegular: require("./assets/fonts/Montserrat-Regular.ttf"),
+    MontserratBold: require("./assets/fonts/Montserrat-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return <RootNavigation />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/navigation/RootNavigation.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MainNavigation from "./MainNavigation";
+
+export default function RootNavigation() {
+  return <MainNavigation />;
+}
