54aa8b148c108ed0b80ca547990f6162c985ad6e
mrpmohiburrahman <mrpmohiburrahman@gmai.com>
2020-10-02 12:15:12 +0600

    Add Log out button in Home.js

--- src/screens/app/Home/Home.js
+++ src/screens/app/Home/Home.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, Text } from "react-native";
+import Button from "../../../components/common/Button";
+import { AuthContext } from "../../../context/AuthContext";
 
 export default function Home() {
+  const {authContest} = useContext(AuthContext)
+  const {signOut} = AuthContext
   return (
     <View>
       <Text>Home</Text>
+      <Button
+        onPress={()=>{
+          signOut()
+        }}
+        title='LOG OUT'
+      />
     </View>
   );
 }
