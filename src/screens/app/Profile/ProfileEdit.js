e8c2fe418fe9fdaf3749d0009b7d3beefa1c2f44
mrpmohiburrahman <mrpmohiburrahman@gmai.com>
2020-10-18 22:25:59 +0600

    Add barebone ProfileEdit.js

---
+++ src/screens/app/Profile/ProfileEdit.js
@@ -0,0 +1,10 @@
+import React, {useState,useEffect} from "react";
+import { View, Text } from "react-native";
+
+
+export default function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>Edit Profile</Text>
+    </View>);
+}
