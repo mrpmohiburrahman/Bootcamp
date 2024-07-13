2c2d9db1cde38f8c3abc9e38f11d4c3d5fa04e12
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-07-14 05:39:06 +0600

    ⬆️ upgrade to latest expo

--- src/theme/common-styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from "react-native";
-import { Colors } from "./index"
-
-export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.white
-    }
-})
--- src/theme/index.js
+++
@@ -1,6 +0,0 @@
-import Metrics from "./metrics";
-import Colors from "./colors";
-import Fonts from "./fonts";
-import Images from "./images";
-
-export { Metrics, Colors, Fonts, Images };
