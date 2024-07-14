edf10f2bae1775bbb754a042901e2c47aaa86cea
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-07-14 07:34:29 +0600

    ✨ add profile tab

--- src/screens/app/Profile/Profile.tsx
+++ src/screens/app/Profile/Profile.tsx
@@ -12,143 +12,157 @@ import { showMessage, hideMessage } from "react-native-flash-message";
 import { useIsFocused } from "@react-navigation/native";
 
 export default function Profile({ navigation }) {
-  return <Text>profile</Text>;
-  // const isFocused = useIsFocused();
-  // const [loading, setLoading]=useState(true)
-  // const [bootcampsLoading, setBootcampsLoading] = useState(true)
-  // const [userData, setUserData]=useState(null)
-  // const [bootcampsData,setBootcampsData]=useState([])
+  // return <Text>profile</Text>;
+  const isFocused = useIsFocused();
+  const [loading, setLoading] = useState(true);
+  const [bootcampsLoading, setBootcampsLoading] = useState(true);
+  const [userData, setUserData] = useState(null);
+  const [bootcampsData, setBootcampsData] = useState([]);
+
+  useEffect(() => {
+    const didFocusSubscription = navigation.addListener("focus", (payload) => {
+      console.debug("focus", payload);
+      getUserData();
+      getUserBootcamps();
+    });
+
+    return didFocusSubscription;
+  }, []);
 
   // useEffect(()=>{
-  //   const didFocusSubscription = navigation.addListener(
-  //     'focus',
-  //     payload => {
-  //       console.debug('focus', payload);
-  //       getUserData()
-  //       getUserBootcamps()
-  //     }
-  //   );
-
-  //   return didFocusSubscription
+  //   getUserBootcamps()
   // },[])
 
-  // // useEffect(()=>{
-  // //   getUserBootcamps()
-  // // },[])
+  const getUserData = async () => {
+    const response = await API.get("auth/profile");
+    setUserData(response.data.data);
+    setLoading(false);
+  };
 
-  // const getUserData = async ()=>{
-  //   const response=await API.get("auth/profile")
-  //   setUserData(response.data.data)
-  //   setLoading(false)
-  // }
+  const getUserBootcamps = async () => {
+    const response = await API.get("bootcamps/getuserbootcamps");
+    setBootcampsData(response.data.data);
+    setBootcampsLoading(false);
+  };
 
-  // const getUserBootcamps = async()=>{
-  //   const response = await API.get("bootcamps/getuserbootcamps")
-  //   setBootcampsData(response.data.data)
-  //   setBootcampsLoading(false)
+  if (loading) {
+    return <Loading />;
+  }
 
-  // }
+  const renderItem = ({ item, index }) => {
+    // console.log('item',item)
+    return (
+      <View
+        style={{
+          borderRadius: 8,
+          padding: 18,
+          backgroundColor: item.coverColor.code,
+          width: Metrics.screenWidth * 0.5,
+          margin: 5,
+        }}>
+        <Text numberOfLines={1} white title centered>
+          {item.title}
+        </Text>
+        <TouchableOpacity
+          onPress={() => {
+            // console.log('item id', item._id  )
+            //setBootcampsLoading
+            API.delete(`bootcamps/${item._id}`).then((res) => {
+              // console.log("res delete", res)
+              if (res.status === 200) {
+                const newList = bootcampsData.filter(
+                  (value) => value._id !== item._id
+                );
+                setBootcampsData(newList);
+                showMessage({
+                  message: "Deleted Bootcamp",
+                  type: "danger",
+                });
+              }
+            });
+          }}
+          style={{ alignSelf: "flex-end", marginTop: 15 }}>
+          <Image source={Images.delete} />
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
-  // if(loading){
-  //   return <Loading/>
-  // }
+  const renderBootcamps = () => {
+    if (bootcampsData.length === 0) {
+      return (
+        <View style={{ margin: Metrics.base }}>
+          <Text bold title>
+            My Bootcamps
+          </Text>
+          <Image source={Images.emptyBootcamps} />
+          <Text centered bold>
+            You have not created any bootcamps yet.
+          </Text>
+        </View>
+      );
+    }
+    return (
+      <View>
+        <Text bold title>
+          My Bootcamps
+        </Text>
+        <FlatList
+          data={bootcampsData}
+          keyExtractor={(item, index) => index.toString()}
+          horizontal={true}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: Metrics.base }}
+          renderItem={renderItem}
+        />
+      </View>
+    );
+  };
+  return (
+    <View style={commonStyles.container}>
+      <View
+        style={{
+          backgroundColor: Colors.green,
+          borderBottomRightRadius: 80,
+          paddingVertical: Metrics.doubleBase,
+          paddingHorizontal: Metrics.base,
+          justifyContent: "space-between",
+          flexDirection: "column",
+        }}>
+        <View>
+          <Text title bold white>
+            My Profile
+          </Text>
+          <View style={{ marginTop: Metrics.base }}>
+            <Text white>{userData.name}</Text>
+            <Text white>{userData.email}</Text>
 
-  // const renderItem = ({item, index})=>{
-  //   // console.log('item',item)
-  //   return(
-  //     <View style={{borderRadius:8, padding:18,backgroundColor:item.coverColor.code,width:Metrics.screenWidth*0.5,margin:5,}}>
-  //       <Text numberOfLines={1} white title centered>
-  //         {item.title}
-  //       </Text>
-  //       <TouchableOpacity onPress={()=>{
-  //         // console.log('item id', item._id  )
-  //         //setBootcampsLoading
-  //         API.delete(`bootcamps/${item._id}`).then((res)=>{
-  //           // console.log("res delete", res)
-  //           if(res.status===200){
-  //             const newList=bootcampsData.filter(
-  //               (value)=>value._id!==item._id
-  //             )
-  //             setBootcampsData(newList)
-  //             showMessage({
-  //               message:'Deleted Bootcamp',
-  //               type: "danger" ,
-  //             })
-  //           }
-  //         })
-  //       }} style={{alignSelf:'flex-end', marginTop:15}}>
-  //         <Image source={Images.delete}/>
-
-  //       </TouchableOpacity>
-  //     </View>
-
-  //   )
-  // }
-
-  // const renderBootcamps=()=>{
-  //   if(bootcampsData.length===0){
-  //     return(
-  //       <View style={{margin:Metrics.base}}>
-  //         <Text bold title>My Bootcamps</Text>
-  //         <Image source={Images.emptyBootcamps}/>
-  //         <Text centered bold>You have not created any bootcamps yet.</Text>
-  //       </View>
-  //     )
-  //   }
-  //   return(
-  //     <View>
-  //       <Text bold title>My Bootcamps</Text>
-  //       <FlatList
-  //         data={bootcampsData}
-  //         keyExtractor={(item,index)=>index.toString()}
-  //         horizontal={true}
-  //         showsVerticalScrollIndicator={false}
-  //         contentContainerStyle={{paddingHorizontal:Metrics.base}}
-  //         renderItem={renderItem}
-  //       />
-  //     </View>
-
-  //   )
-
-  // }
-  // return (
-  //   <View style={commonStyles.container}>
-  //     <View style={{
-  //       backgroundColor:Colors.green,
-  //       borderBottomRightRadius:80,
-  //       paddingVertical:Metrics.doubleBase,
-  //       paddingHorizontal:Metrics.base,
-  //       justifyContent:'space-between',
-  //       flexDirection:'column',
-
-  //       }}>
-  //       <View>
-  //         <Text title bold white>My Profile</Text>
-  //         <View style={{marginTop:Metrics.base}}>
-  //           <Text white>{userData.name}</Text>
-  //           <Text white>{userData.email}</Text>
-
-  //           <View style={{marginTop:Metrics.halfBase}}>
-  //             <Text white>
-  //             {userData.bio}
-  //             </Text>
-  //           </View>
-  //         </View>
-  //       </View>
-  //       <View style={{alignSelf:''}}>
-  //         <Button  style={{
-  //           backgroundColor:Colors.green,
-  //           borderColor:'#18B18D',
-  //           width:165,
-  //           height:32,
-  //           marginTop:20
-  //           }} title='Edit Profile' onPress={() => navigation.navigate('ProfileEdit',{email:userData.email,bio:userData.bio,name:userData.name})}/>
-  //       </View>
-
-  //     </View>
-  //     <View>
-  //       {bootcampsLoading?<Loading/>:renderBootcamps()}
-  //     </View>
-  //   </View>
-  // );
+            <View style={{ marginTop: Metrics.halfBase }}>
+              <Text white>{userData.bio}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ alignSelf: "" }}>
+          <Button
+            style={{
+              backgroundColor: Colors.green,
+              borderColor: "#18B18D",
+              width: 165,
+              height: 32,
+              marginTop: 20,
+            }}
+            title="Edit Profile"
+            onPress={() =>
+              navigation.navigate("ProfileEdit", {
+                email: userData.email,
+                bio: userData.bio,
+                name: userData.name,
+              })
+            }
+          />
+        </View>
+      </View>
+      <View>{bootcampsLoading ? <Loading /> : renderBootcamps()}</View>
+    </View>
+  );
 }
